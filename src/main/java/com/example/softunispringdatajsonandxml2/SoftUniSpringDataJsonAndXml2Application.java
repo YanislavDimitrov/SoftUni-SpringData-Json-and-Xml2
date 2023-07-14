c5796530e992b7f09865a1f49276e814add728c8
Yanislav Dimitrov <yanislav.dimitrov1994@bav.bg>
2023-07-14 12:58:52 +0300

    initial commit

---
+++ src/main/java/com/example/softunispringdatajsonandxml2/SoftUniSpringDataJsonAndXml2Application.java
@@ -0,0 +1,13 @@
+package com.example.softunispringdatajsonandxml2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftUniSpringDataJsonAndXml2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftUniSpringDataJsonAndXml2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/softunispringdatajsonandxml2/SoftUniSpringDataJsonAndXml2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softunispringdatajsonandxml2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftUniSpringDataJsonAndXml2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
